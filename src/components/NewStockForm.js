24ba6bc43daf41c6e307cae329e078e56a5f1d84
Alvin <alvinnwong@gmail.com>
2019-10-21 12:25:48 +1100

    Added stock class for global quote

--- src/components/NewStockForm.js
+++ src/components/NewStockForm.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-
 class NewStockForm extends React.Component {
   render() {
+    let AV = window.alphavantage.default({ key: 'Q57E765E5P4NIGUH' });
+    let data = AV.data.quote('msft').then(data => {
+      console.log(data);
+    });
     return (
       <form>
         <h1>Hello</h1>
         <p>Enter the Code of the stock name:</p>
+        <script src="../node_modules/alphavantage/dist/bundle.js"></script>
         <input
           type="text"
         />
