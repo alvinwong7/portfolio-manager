5adc8ba8ed040a35ca267e31cfbf141ebba84e4d
Alvin <alvinnwong@gmail.com>
2019-11-04 13:52:28 +1100

    Working porfolio overview and table

--- src/components/PortfolioOverview.js
+++ src/components/PortfolioOverview.js
@@ -1,6 +1,43 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 // Top level component of portfolio page
 class PortfolioOverview extends React.Component {
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            userStocks : this.props.userStocks,
+            networth : '0',
+        }
+    }
+
+    evalNetworth() {
+        let value = 0
+        let stocks = this.state.userStocks
+        Object.keys(this.state.userStocks).forEach(function(key) {
+            value += parseFloat(stocks[key]['value'])
+        });
+        this.setState({
+            networth : value.toString()
+        })
+    }
+
+    componentDidMount() {
+        this.evalNetworth()
+    }
+
+    render() {
+        return (
+            <Card>
+            <Card.Header>Personal Portfolio Performance</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                {this.state.networth}
+                </Card.Text>
+            </Card.Body>
+            </Card>
+        )
+    }
 }
+
+export { PortfolioOverview }
--- src/components/PurchasedStock.js
+++ src/components/PurchasedStock.js
@@ -2,5 +2,33 @@ import React from 'react'
 
 // Rows for portfolio table stock summary
 class PurchasedStock extends React.Component {
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            name : this.props.name,
+            price : this.props.userStocks['price'],
+            profit : this.props.userStocks['profits/loss'],
+            units : this.props.userStocks['units'],
+            changePercent : this.props.userStocks['changePercent'],
+            value : this.props.userStocks['value'],
+            weight : this.props.userStocks['weight'],
+        }
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.state.name}</td>
+                <td>{this.state.price}</td>
+                <td>{this.state.profit}</td>
+                <td>{this.state.units}</td>
+                <td>{this.state.changePercent}</td>
+                <td>{this.state.value}</td>
+                <td>{this.state.weight}</td>
+            </tr>
+        )
+    }
 }
+
+export { PurchasedStock }
