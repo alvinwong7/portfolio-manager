442d95cae6177bd1991ae34825dd9d0edf00ea68
Alvin <alvinnwong@gmail.com>
2019-11-05 17:07:03 +1100

    Garbage commit

--- src/components/PortfolioOverview.js
+++ src/components/PortfolioOverview.js
@@ -4,11 +4,18 @@ import { Card } from 'react-bootstrap'
 // Top level component of portfolio page
 class PortfolioOverview extends React.Component {
     constructor(props) {
+<<<<<<< HEAD
         super(props)
         this.state = {
             userStocks : this.props.userStocks,
             networth : '0',
         }
+=======
+    super(props)
+    this.addStock = this.addStock.bind(this);
+    this.state = {
+      stocks : ['MSFT']//, 'AMZN', 'GOOGL'],
+>>>>>>> parent of a7affba... cleaning up login and portfolio
     }
 
     evalNetworth() {
@@ -40,4 +47,8 @@ class PortfolioOverview extends React.Component {
     }
 }
 
+<<<<<<< HEAD
 export { PortfolioOverview }
+=======
+export{ PortfolioOverview }
+>>>>>>> parent of a7affba... cleaning up login and portfolio
