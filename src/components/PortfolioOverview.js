4fca37ff2f82649ffde9ad67aae2bb92939fab4b
Alvin <alvinnwong@gmail.com>
2019-11-05 17:08:55 +1100

    trying to revert

--- src/components/PortfolioOverview.js
+++ src/components/PortfolioOverview.js
@@ -3,52 +3,41 @@ import { Card } from 'react-bootstrap'
 
 // Top level component of portfolio page
 class PortfolioOverview extends React.Component {
-    constructor(props) {
-<<<<<<< HEAD
-        super(props)
-        this.state = {
-            userStocks : this.props.userStocks,
-            networth : '0',
-        }
-=======
+  constructor(props) {
     super(props)
-    this.addStock = this.addStock.bind(this);
     this.state = {
-      stocks : ['MSFT']//, 'AMZN', 'GOOGL'],
->>>>>>> parent of a7affba... cleaning up login and portfolio
+      userStocks : this.props.userStocks,
+      networth : '0',
     }
+  }
 
-    evalNetworth() {
-        let value = 0
-        let stocks = this.state.userStocks
-        Object.keys(this.state.userStocks).forEach(function(key) {
-            value += parseFloat(stocks[key]['value'])
-        });
-        this.setState({
-            networth : value.toString()
-        })
-    }
+  evalNetworth() {
+    let value = 0
+    let stocks = this.state.userStocks
+    Object.keys(this.state.userStocks).forEach(function(key) {
+      value += parseFloat(stocks[key]['value'])
+    });
+    this.setState({
+      networth : value.toString()
+    })
+  }
 
-    componentDidMount() {
-        this.evalNetworth()
-    }
+  componentDidMount() {
+    this.evalNetworth()
+  }
 
-    render() {
-        return (
-            <Card>
-            <Card.Header>Personal Portfolio Performance</Card.Header>
-            <Card.Body>
-                <Card.Text>
-                {this.state.networth}
-                </Card.Text>
-            </Card.Body>
-            </Card>
-        )
-    }
+  render() {
+    return (
+      <Card>
+      <Card.Header>Personal Portfolio Performance</Card.Header>
+      <Card.Body>
+        <Card.Text>
+        {this.state.networth}
+        </Card.Text>
+      </Card.Body>
+      </Card>
+    )
+  }
 }
 
-<<<<<<< HEAD
 export { PortfolioOverview }
-=======
-export{ PortfolioOverview }
->>>>>>> parent of a7affba... cleaning up login and portfolio
