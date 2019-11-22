14bfd650573afc38d92e57b31a8a6b68d1d2b0be
Alvin <alvinnwong@gmail.com>
2019-11-23 01:47:04 +1100

    Fixed stock plot + made it faster

--- src/components/StockPlot.js
+++ src/components/StockPlot.js
@@ -13,91 +13,113 @@ class StockPlot extends React.Component{
         const filterTaps = 2 * filterExtent + 1
 
         this.state = {
-            Name: props.stockName,
-            Period: props.years,
+            name: props.stockName,
+            period: props.years,
             filterExtent: filterExtent,
             filterTaps: filterTaps,
-            HistDate: [],
-            HistHigh: [],
-            HistLow: [],
-            HistAvg: [],
-            HistMovAvg: [],
+            histDate: [],
+            histHigh: [],
+            histLow: [],
+            histAvg: [],
+            histMovAvg: [],
+            update: true,
         }
+        
+        this.getInfo = this.getInfo.bind(this)
+        this.getInfo()
+    }
 
+    UNSAFE_componentWillReceiveProps = (nextProps) => {
+        this.setState({
+            name: nextProps.stockName,
+            period: nextProps.years,
+            update: true
+        })
+        this.getInfo()
+    }
 
+    getInfo = () => {
         // Access stock data from AlphaVantage API (5 calls per minute)
         const key = '8ITU7LH4G30XUCNF'
         const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol='
-                    + this.state.Name + '&apikey=' + key
+                    + this.state.name + '&apikey=' + key
 
-        axios
-            .get(url)
-            .then( response => {
 
-                let TimeSeries = response.data['Time Series (Daily)']
-                let movAvg = 0
+        axios.get(url).then( response => {
+            let TimeSeries = response.data['Time Series (Daily)']
+            let movAvg = 0
 
-                // Collect historical stock data over previous three years
-                for (let i = 0; i < this.state.Period*365 && i < Object.keys(TimeSeries).length; i++){
+            let histDate = []
+            let histHigh = []
+            let histLow = []
+            let histAvg = []
+            let histMovAvg = []
+            // Collect historical stock data over previous three years
+            for (let i = 0; i < this.state.period*365 && i < Object.keys(TimeSeries).length; i++) {
 
-                    let date = Object.keys(TimeSeries)[i]
-                    let high = parseFloat(TimeSeries[date]['2. high'])
-                    let low = parseFloat(TimeSeries[date]['3. low'])
-                    let avg = 0.5 * (high + low)
+                let date = Object.keys(TimeSeries)[i]
+                let high = parseFloat(TimeSeries[date]['2. high'])
+                let low = parseFloat(TimeSeries[date]['3. low'])
+                let avg = 0.5 * (high + low)
+                // Append historical high and low prices
+                histDate.push(date)
+                histHigh.push(high)
+                histLow.push(low)
+                histAvg.push(avg)
 
-                    // Append historical high and low prices
-                    this.setState({
-                        HistDate: this.state.HistDate.concat(date),
-                        HistHigh: this.state.HistHigh.concat(high),
-                        HistLow: this.state.HistLow.concat(low),
-                        HistAvg: this.state.HistAvg.concat(avg)
-                    })
-
-                    if (i < this.state.filterTaps) {
-                        movAvg += avg
-                    } else {
-                        movAvg = movAvg - this.state.HistAvg[i-this.state.filterTaps] + avg
-                        this.setState({
-                            HistMovAvg: this.state.HistMovAvg.concat(movAvg/filterTaps),
-                        })
-                    }
-
+                if (i < this.state.filterTaps) {
+                    movAvg += avg
+                } else {
+                    movAvg = movAvg - histAvg[i-this.state.filterTaps] + avg
+                    histMovAvg.push(movAvg/this.state.filterTaps)
                 }
-
+            }
+            this.setState({
+                histDate: histDate,
+                histHigh: histHigh,
+                histLow: histLow,
+                histAvg: histAvg,
+                histMovAvg: histMovAvg,
+                update: false
             })
-            .catch( error => {
+            }).catch( error => {
                 console.log(error)
             })
-        
     }
 
 
     render = () => {
-        return(
+        console.log(this.state)
+        if (this.state.update === true) {
+            return (
+                <div></div>
+            )
+        }
+        return (
             <div>
                 <br/>
 
                 <Plot
                     data={[
                         {
-                            x: this.state.HistDate,
-                            y: this.state.HistHigh,
+                            x: this.state.histDate,
+                            y: this.state.histHigh,
                             type: 'scatter',
                             mode: 'lines',
                             name: 'High ($)',
                             marker: {color: 'green'},
                         },
                         {
-                            x: this.state.HistDate,
-                            y: this.state.HistLow,
+                            x: this.state.histDate,
+                            y: this.state.histLow,
                             type: 'scatter',
                             mode: 'lines',
                             name: 'Low ($)',
                             marker: {color: 'red'},
                         },
                         {
-                            x: this.state.HistDate.slice(this.state.filterTaps-this.state.filterExtent,this.state.HistDate.length),
-                            y: this.state.HistMovAvg,
+                            x: this.state.histDate.slice(this.state.filterTaps-this.state.filterExtent,this.state.histDate.length),
+                            y: this.state.histMovAvg,
                             type: 'scatter',
                             mode: 'lines',
                             name: 'Trendline',
@@ -108,7 +130,7 @@ class StockPlot extends React.Component{
                         {
                             width: 900,
                             height: 450,
-                            title: 'Price History',
+                            title: 'Price history',
                         }
                     }
                 />
